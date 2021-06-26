8714ede0ffa441df5d1734f35645f6b8028c5854
Yuxuan Xie <teststaybaka@gmail.com>
2021-06-26 18:59:21 +0000

    Add & Revise API

--- apis.ts
+++ apis.ts
@@ -1,6 +1,10 @@
 declare var argv: Array<string>;
-declare function screenshot(path: string, type: string, quality?: number): Promise<string|Buffer>;
-declare function fileExists(path: string): Promise<void>;
-declare function deleteFile(path: string): Promise<void>;
-declare function setViewport(width: number, height: number): Promise<void>; 
-
+declare function exit(): void;
+declare function screenshot(
+  relativePath: string,
+  options: { delay?: number; fullPage?: boolean; quality?: number }
+): Promise<string>;
+declare function fileExists(relativePath: string): Promise<void>;
+declare function readFile(relativePath: string): Promise<string>;
+declare function deleteFile(relativePath: string): Promise<void>;
+declare function setViewport(width: number, height: number): Promise<void>;
